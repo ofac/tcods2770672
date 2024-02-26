7c1ad35805113494d3c1b65c857a51e92ab6aeae
ofac <ofaczero@gmail.com>
2024-02-26 13:49:19 -0500

    Gap between inputs

---
+++ 02-pdo/db/petsapp.sql
@@ -0,0 +1,77 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: localhost
+-- Tiempo de generación: 23-02-2024 a las 18:22:21
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `petsapp`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pets`
+--
+
+CREATE TABLE `pets` (
+  `id` int(11) NOT NULL,
+  `name` varchar(32) NOT NULL,
+  `photo` varchar(64) NOT NULL DEFAULT 'images/photo-pet.png',
+  `kind` varchar(32) NOT NULL,
+  `weight` int(3) NOT NULL,
+  `age` int(3) NOT NULL,
+  `breed` varchar(32) NOT NULL,
+  `location` varchar(64) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `pets`
+--
+
+INSERT INTO `pets` (`id`, `name`, `photo`, `kind`, `weight`, `age`, `breed`, `location`) VALUES
+(1, 'Firulais', 'ico-pet.svg', 'Dog', 25, 3, 'Galgo', 'Manizales'),
+(2, 'Michi', 'ico-pet.svg', 'Cat', 8, 2, 'Persa', 'Pereira'),
+(3, 'Toby', 'ico-pet.svg', 'Dog', 18, 5, 'Puddle', 'Armenia'),
+(4, 'Pocholo', '1708697222.png', 'Dog', 10, 2, 'Pug', 'Manizales'),
+(5, 'Mireya', '1708697962.png', 'Cat', 6, 3, 'Munchkin', 'Pereira'),
+(6, 'Loki', '1708702925.png', 'Dog', 12, 4, 'Pitbull', 'Cali'),
+(7, 'Sadman', '1708703205.png', 'Cat', 3, 3, 'Persa', 'Cali');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `pets`
+--
+ALTER TABLE `pets`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `pets`
+--
+ALTER TABLE `pets`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
