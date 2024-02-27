ad2ff5971c7a34fba5162a3c3f82ac3ecbc9fcd8
ofac <ofaczero@gmail.com>
2024-02-27 11:46:07 -0500

    Dashboard & close session

--- 03-sessions/dashboard.php
+++ 03-sessions/dashboard.php
@@ -10,15 +10,55 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
     <link rel="stylesheet" href="<?php echo URLCSS . "/master.css" ?>">
+    <style>
+        div.menu {
+            background-color: var(--color-pri);
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            gap: 2rem;
+            position: absolute;
+            top: -800px;
+            opacity: 0;
+            left: 0;
+            z-index: 999;
+            justify-content: center;
+            min-height: 100vh;
+            transition: all 0.5s ease-in;
+            width: 100%;
+
+            a:is(:link, :visited) {
+                border: 1px solid #fff;
+                border-radius: 50px;
+                color: #fff;
+                font-size: 2rem;
+                padding: 10px 20px;
+                text-decoration: none;
+            }
+        }
+        div.menu.open {
+            top: 0;
+            opacity: 1;
+        }
+
+    </style>
 </head>
 <body>
+
+<div class="menu">
+    <a href="javascript:;" class="closem">X</a>
+    <nav>
+        <a href="close.php">Close Session</a>
+    </nav>
+</div>
+
 <main>
         <header class="nav level-0">
             <a href="">
                 <img src="<?php echo URLIMGS . "/ico-back.svg" ?>" alt="Back">
             </a>
             <img src="<?php echo URLIMGS . "/logo.svg" ?>" alt="Logo">
-            <a href="" class="mburger">
+            <a href="javascript:;" class="mburger">
                 <img src="<?php echo URLIMGS . "/mburger.svg" ?>" alt="Menu Burger">
             </a>
         </header>
@@ -52,6 +92,16 @@
     <script src="<?php echo URLJS . "/jquery-3.7.1.min.js" ?>"></script>
     <script>
         $(document).ready(function () {
+
+
+            $('body').on('click', '.mburger',  function () {
+                $('.menu').addClass('open')
+            })
+            $('body').on('click', '.closem',  function () {
+                $('.menu').removeClass('open')
+            })
+
+
             <?php if(isset($_SESSION['msg'])): ?>
                 Swal.fire({
                     position: "top-end",
